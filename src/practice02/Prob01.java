ce6b0efa25503358cc58540b118caac6d99cf445
wns8628 <wns8628>
2018-12-27 17:20:06 +0900

    1번끝

--- src/practice02/Prob01.java
+++ src/practice02/Prob01.java
@@ -1,9 +1,20 @@
 package practice02;
 
+import java.util.Scanner;
+
 public class Prob01 {
-
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner( System.in );
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
-	}
-
+	
+		int money;
+		System.out.print("금액 : ");
+		money = scanner.nextInt();
+		
+		for(int i = 0; i< MONEYS.length; i++) {
+			System.out.println( MONEYS[i] + "원 " + money/MONEYS[i] + "개");
+			money %= MONEYS[i];
+		}
+		
+	}	
 }
